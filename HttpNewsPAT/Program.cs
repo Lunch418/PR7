862b7382491c245cd68ef8bbf9838e9d10815926
Артем Сыпачев <Lunch418@bk.ru>
2025-12-10 23:54:50 +0500

    SingIn.Сыпачев

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -12,17 +13,33 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
+            SingIn("user", "user");
+            Console.Read();
+        }
+        public static void SingIn(string Login, string Password)
+        {
+            string url = "http://news.permaviat.ru/ajax/login.php";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = new CookieContainer();
+
+            string postData = $"Login={Login}&password={Password}";
+            byte[] Data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = Data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+            }
+
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Console.WriteLine(response.StatusDescription);
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
+            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+
+            string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
             Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-            Console.Read(); 
         }
     }
 }
