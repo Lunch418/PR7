48365ddea3415aef4ac3283bbd7b621979b36234
Krukov <da123@mail.ru>
2025-12-12 14:50:24 +0500

    На 5

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,103 +1,222 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Linq;
-using System.Collections;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
-namespace SimpleNewsParser
+namespace HttpNewsPAT
 {
-    internal class Program
+    public class Program
     {
+        private static HttpClient httpClient = new HttpClient();
+        private static string Token;
+
         static void Main(string[] args)
         {
-            Cookie token = SingIn("user", "user");
-            string Content = GetContent(token);
-            ParsingHtml(Content);
-            //WebRequest request = WebRequest.Create("https://news.permaviat.ru/main");
-            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            //Console.WriteLine(response.StatusDescription);
-            //using (Stream dataStream = response.GetResponseStream()) 
-            //{
-            //    using (StreamReader reader = new StreamReader(dataStream))
-            //    {
-            //        string responseFromServer = reader.ReadToEnd();
-            //        Console.WriteLine(responseFromServer);
-            //    }
-            //}              
-
-
-            ////reader.Close();
-            ////dataStream.Close();
-            ////response.Close();
-            Console.Read();
-
-        }
-        public static Cookie SingIn(string login, string password)
-        {
-            Cookie token = null;
-            string Url = "http://news.permaviat.ru/ajax/login.php";
-
-            Debug.WriteLine($"Выполняем запрос: {Url}");
-
-            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(Url);
-            Request.Method = "POST";
-            Request.ContentType = "application/x-www-form-urlencoded";
-            Request.CookieContainer = new CookieContainer();
-
-            byte[] Data = Encoding.ASCII.GetBytes($"Login={login}&password={password}");
-            Request.ContentLength = Data.Length;
-
-            using (Stream stream = Request.GetRequestStream())
+            Debug.Listeners.Add(new TextWriterTraceListener("DebugLog.txt"));
+            Help();
+            while (true)
             {
-                stream.Write(Data, 0, Data.Length);
+                SetComand();
             }
-
-            using (HttpWebResponse Response = (HttpWebResponse)Request.GetResponse())
-            {
-                Debug.WriteLine($"Статус выполнения: {Response.StatusCode}");
-
-                string ResponseFromServer = new StreamReader(Response.GetResponseStream()).ReadToEnd();
-                token = Response.Cookies["token"];
-            }
-            return token;
-        }
-        public static string GetContent(Cookie token)
-        {
-            string Content = null;
-            string Url = "http://news.permaviat.ru/main";
-            Debug.WriteLine($"Выполняем запрос: {Url}");
-
-            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(Url);
-            Request.CookieContainer = new CookieContainer();
-            Request.CookieContainer.Add(token);
-
-            using (HttpWebResponse Response = (HttpWebResponse)Request.GetResponse())
-            {
-                Debug.WriteLine($"Статус выполнения: {Response.StatusCode}");
-
-                Content = new StreamReader(Response.GetResponseStream()).ReadToEnd();
-
-            }
-            return Content;
         }
         public static void ParsingHtml(string htmlCode)
         {
-            var Html = new HtmlDocument();
-            Html.LoadHtml(htmlCode);
-
-            var Document = Html.DocumentNode;
-            IEnumerable DivsNews = Document.Descendants(0).Where(n => n.HasClass("news"));
-            foreach (HtmlNode DivNews in DivsNews)
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(htmlCode);
+            HtmlNode document = html.DocumentNode;
+            IEnumerable<HtmlNode> divsNews = document.Descendants().Where(n => n.HasClass("news"));
+            string content = "";
+            foreach (HtmlNode divNews in divsNews)
             {
-                var src = DivNews.ChildNodes[1].GetAttributeValue("src", "нет изображения");
+                string src = divNews.ChildNodes[1].GetAttributeValue("src", "none");
+                string name = divNews.ChildNodes[3].InnerText;
+                string description = divNews.ChildNodes[5].InnerText;
+                content += $"{name}\nИзображение: {src}\nОписание: {description}\n";
+            }
+            Console.Write(content);
+        }
+        public static async void AddNewPost()
+        {
+            if (!string.IsNullOrEmpty(Token))
+            {
+                string name;
+                string description;
+                string image;
+                Console.WriteLine("Заголовок новости:");
+                name = Console.ReadLine();
+                Console.WriteLine("Текст новости");
+                description = Console.ReadLine();
+                Console.WriteLine("Ссылка на изображение:");
+                image = Console.ReadLine();
+                string url = "http://10.111.20.114/ajax/add.php";
+                WriteLog($"Выполнение запроса: {url}");
+                var postData = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("name", name),
+                    new KeyValuePair<string, string>("description", description),
+                    new KeyValuePair<string, string>("src", image),
+                    new KeyValuePair<string, string>("token", Token)
+                });
+                HttpResponseMessage response = await httpClient.PostAsync(url, postData);
+                WriteLog($"Статус выполнения: {response.StatusCode}");
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Запрос выполнен успешно");
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка выполнения запроса: {response.StatusCode}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка выполнения запроса: пользователь не авторизован");
+            }
+        }
+        public static void WriteLog(string debugContent)
+        {
+            Debug.WriteLine(debugContent);
+            Debug.Flush();
+        }
+        public static async Task SignIn(string Login, string Password)
+        {
+            string url = "http://10.111.20.114/ajax/login.php";
+            WriteLog($"Выполнение запроса: {url}");
+            var postData = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("login", Login),
+                new KeyValuePair<string, string>("password", Password)
+            });
+            HttpResponseMessage response = await httpClient.PostAsync(url, postData);
+            WriteLog($"Статус выполнения: {response.StatusCode}");
+            if (response.IsSuccessStatusCode)
+            {
+                string cookies = response.Headers.GetValues("Set-Cookie").FirstOrDefault();
+                if (!string.IsNullOrEmpty(cookies))
+                {
+                    Token = cookies.Split(';')[0].Split('=')[1];
+                    Console.WriteLine("успешная авторизация");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка выполнения запроса: {response.StatusCode}");
+            }
+        }
+        public static async Task<string> GetContent()
+        {
+            if (!string.IsNullOrEmpty(Token))
+            {
+                string url = "http://10.111.20.114/main";
+                WriteLog($"Выполнение запроса: {url}");
+                httpClient.DefaultRequestHeaders.Add("token", Token);
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                WriteLog($"Статус выполнения: {response.StatusCode}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка выполнения запроса: {response.StatusCode}");
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка выполнения запроса: не авторизован");
+                return string.Empty;
+            }
+        }
+        static void Help()
+        {
+            Console.Write("/SignIn");
+            Console.WriteLine(" (авторизация на сайте)");
+            Console.Write("/Posts");
+            Console.WriteLine(" (вывод всех постов на сайте)");
+            Console.Write("/Add");
+            Console.WriteLine(" (добавление новой записи)");
+            Console.Write("/Lenta");
+            Console.WriteLine(" (парсинг новостей с Lenta.ru)");
+        }
+        static async void SetComand()
+        {
+            try
+            {
+                string Command = Console.ReadLine();
+                if (Command.Contains("/SignIn")) await SignIn("user", "user");
+                if (Command.Contains("/Posts")) ParsingHtml(await GetContent());
+                if (Command.Contains("/Add")) AddNewPost();
+                if (Command.Contains("/Lenta")) await ParseLentaRu();
 
-                var name = DivNews.ChildNodes.Count > 3 ? DivNews.ChildNodes[3].InnerHtml : "нет названия";
-                var description = DivNews.ChildNodes.Count > 5 ? DivNews.ChildNodes[5].InnerHtml : "нет описания";
-                Console.WriteLine(name + "\n" + "Изображение" + src + "\n" + "Описание:" + description + "\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Request error: " + ex.Message);
+            }
+        }
+        public static async Task ParseLentaRu()
+        {
+            try
+            {
+                // URL главной страницы Lenta.ru
+                string url = "https://lenta.ru/";
+                WriteLog($"Начинается парсинг Lenta.ru: {url}");
+
+                // Отправляем GET-запрос к сайту
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                WriteLog($"Статус выполнения запроса к Lenta.ru: {response.StatusCode}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string htmlCode = await response.Content.ReadAsStringAsync();
+                    HtmlDocument html = new HtmlDocument();
+                    html.LoadHtml(htmlCode);
+                    HtmlNode document = html.DocumentNode;
+
+                    var newsNodes = document.SelectNodes("//a[contains(@class, 'card')] | //a[contains(@class, 'item')]");
+
+                    string parsedContent = "";
+
+                    if (newsNodes != null)
+                    {
+                        foreach (var newsItem in newsNodes.Take(10)) 
+                        {
+                            string title = WebUtility.HtmlDecode(newsItem.InnerText?.Trim() ?? "");
+                            string link = newsItem.GetAttributeValue("href", "");
+                            if (!string.IsNullOrEmpty(link) && !link.StartsWith("http"))
+                            {
+                                link = new Uri(new Uri(url), link).AbsoluteUri;
+                            }
+
+                            parsedContent += $"{title}\nСсылка: {link}\n\n";
+                        }
+
+                        Console.WriteLine("=== Последние новости с Lenta.ru ===\n");
+                        Console.Write(parsedContent);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не удалось найти новости на странице. Возможно, изменилась структура сайта.");
+                        WriteLog("Не удалось найти элементы новостей по заданным селекторам.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при запросе к Lenta.ru: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при парсинге Lenta.ru: {ex.Message}");
+                WriteLog($"Ошибка в ParseLentaRu: {ex.ToString()}");
             }
         }
     }
